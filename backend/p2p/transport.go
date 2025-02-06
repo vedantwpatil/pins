04cd259f44b22d084188daa548ce7fb84bc169c6
Vedant Patil <vedant.waman.patil@gmail.com>
2025-02-05 23:51:36 -0500

    Started building p2p

---
+++ backend/p2p/transport.go
@@ -0,0 +1,7 @@
+package p2p
+
+// Peer is the interface which represents the remote node/remote connection
+type Peer interface{}
+
+// Transport is what handles the communication between nodes in the network. This can be of the form (TCP, UDP, websockets ...)
+type Transport interface{}
